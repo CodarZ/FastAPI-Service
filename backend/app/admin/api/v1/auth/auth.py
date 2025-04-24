a22fba9ce4039c6a65aef81ecc0613b3b9d79908
CodarZ <100060763+CodarZ@users.noreply.github.com>
2025-04-24 10:06:50 +0800

    feat: 移除 Swagger 登录功能及相关依赖

--- backend/app/admin/api/v1/auth/auth.py
+++ backend/app/admin/api/v1/auth/auth.py
@@ -1,19 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from typing import Annotated
 
-from fastapi import APIRouter, Depends
-from fastapi.security import HTTPBasicCredentials
-
-from backend.app.admin.schema.token import SwaggerToken
-from backend.app.admin.service.auth import auth_service
-from backend.common.response.base import ResponseModel
+from fastapi import APIRouter
 
 router = APIRouter()
-
-
-@router.post('/login/swagger', summary='swagger 调试', description='用于快捷获取 token 进行 swagger 认证')
-async def swagger_login(obj: Annotated[HTTPBasicCredentials, Depends()]):
-    token, user = await auth_service.swagger_login(obj=obj)
-    return ResponseModel(data=SwaggerToken(access_token=token, token_type='Bearer', user=user))
--- backend/app/admin/service/auth.py
+++ backend/app/admin/service/auth.py
@@ -2,38 +2,11 @@
 # -*- coding: utf-8 -*-
 
 
-from fastapi.security import HTTPBasicCredentials
-
-from backend.app.admin.schema.user import UserInfo
-from backend.common.security.jwt import create_access_token
-from backend.utils.timezone import timezone
-
 __all__ = ['auth_service']
 
 
 class AuthService:
     """认证服务类"""
-
-    async def swagger_login(self, *, obj: HTTPBasicCredentials):
-        """Swagger 登录"""
-
-        # TODO 查询 user 数据库
-        a_token = await create_access_token(
-            1,
-            False,
-            # extra info
-            swagger=True,
-        )
-        return a_token.access_token, UserInfo(
-            id=1,
-            username=obj.username,
-            nickname='Swagger',
-            avatar='https://example.com/avatar.png',
-            email=None,
-            phone=None,
-            uuid='12345678-1234-5678-1234-567812345678',
-            birth_date=timezone.now(),
-        )
 
 
 auth_service = AuthService()
