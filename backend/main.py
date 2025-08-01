65839e84a82d487383f3c764d83223113a4e4c64
CodarZ <1476712061@qq.com>
2025-08-01 20:42:49 +0800

    clear

--- backend/main.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from backend.core.registrar import register_app
-
-app = register_app()
--- backend/utils/tools.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-import tomllib
-
-
-def get_project_version(pyproject_path: str = 'pyproject.toml') -> str:
-    with open(pyproject_path, 'rb') as f:
-        return tomllib.load(f)['project']['version']
