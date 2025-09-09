9bbc246c8814837693c3df663866a3210a33a22f
CodarZ <1476712061@qq.com>
2025-09-09 21:34:09 +0800

    ✨ feat(core/registrar.py): 新增 CORS 中间件

--- backend/core/registrar.py
+++ backend/core/registrar.py
@@ -3,16 +3,32 @@
 
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI as FastAPIBase
 from fastapi_limiter import FastAPILimiter, http_default_callback
+from starlette.middleware.cors import CORSMiddleware
+from starlette.types import ASGIApp
 
 from backend.common.log import set_custom_logfile, setup_logging
 from backend.core.config import settings
 from backend.database.postgresql import create_tables
 from backend.database.redis import redis_client
+from backend.utils.serializers import MsgSpecJSONResponse
 
 
-def register_app() -> FastAPI:
+def register_app() -> FastAPIBase:
+    class FastAPI(FastAPIBase):
+        # https://github.com/fastapi/fastapi/discussions/7847
+        # https://github.com/fastapi/fastapi/discussions/8027
+        def build_middleware_stack(self) -> ASGIApp:
+            return CORSMiddleware(
+                super().build_middleware_stack(),
+                allow_credentials=True,
+                allow_methods=['*'],
+                allow_headers=['*'],
+                allow_origins=settings.CORS_ALLOWED_ORIGINS,
+                expose_headers=settings.CORS_EXPOSE_HEADERS,
+            )
+
     app = FastAPI(
         title=settings.FASTAPI_TITLE,
         version=settings.FASTAPI_VERSION,
@@ -20,6 +36,7 @@ def register_app() -> FastAPI:
         openapi_url=settings.FASTAPI_OPENAPI_URL,
         docs_url=settings.FASTAPI_DOCS_URL,
         lifespan=init,
+        default_response_class=MsgSpecJSONResponse,
     )
 
     register_logger()
@@ -28,7 +45,7 @@ def register_app() -> FastAPI:
 
 
 @asynccontextmanager
-async def init(app: FastAPI):
+async def init(app: FastAPIBase):
     # 创建数据库表
     await create_tables()
 
