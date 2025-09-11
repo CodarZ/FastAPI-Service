d56ae15928ed534a00dbfe9e498aa31c7a3683bb
CodarZ <1476712061@qq.com>
2025-09-11 14:19:51 +0800

    ✨ feat: 新增多个文件模块的初始化文件

---
+++ backend/app/admin/api/router.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from fastapi import APIRouter
+
+admin_router = APIRouter()
--- backend/app/router.py
+++ backend/app/router.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 from fastapi import APIRouter
 
+from backend.app.admin.api.router import admin_router
+
 router = APIRouter()
+
+router.include_router(admin_router)
