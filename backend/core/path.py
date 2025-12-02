cbb9a85b4ac9f7d46b47cea4c1fcb0419b43d71e
CodarZ <1476712061@qq.com>
2025-12-02 17:28:15 +0800

    🔧 chore: 新增项目基础配置信息、环境变量

---
+++ backend/core/path.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+# 根目录
+ROOT_PATH = Path(__file__).resolve().parent.parent.parent
+
+# 项目目录
+BASE_PATH = ROOT_PATH / 'backend'
+
+# 日志文件目录
+LOG_DIR = ROOT_PATH / 'log'
+
+# 静态资源目录
+STATIC_DIR = ROOT_PATH / 'static'
+UPLOAD_DIR = STATIC_DIR / 'upload'
+
+# env 环境变量目录
+ENV_DIR = ROOT_PATH / 'env'
+
+# 确保目录存在
+for directory in [LOG_DIR, STATIC_DIR, UPLOAD_DIR, ENV_DIR]:
+    directory.mkdir(parents=True, exist_ok=True)
