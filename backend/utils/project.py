a6e1e63f57cd3c2818f67fa043bc93a3de076e9a
CodarZ <1476712061@qq.com>
2025-12-02 17:27:12 +0800

    ✨ feat(utils): 新增工具: 从 pyproject.toml 读取项目版本号

---
+++ backend/utils/project.py
@@ -0,0 +1,21 @@
+"""
+项目信息工具
+"""
+
+import tomllib
+
+from backend.core.path import ROOT_PATH
+
+
+def get_project_version() -> str:
+    """从 pyproject.toml 读取项目版本号"""
+    pyproject_toml_path = ROOT_PATH / 'pyproject.toml'
+    if pyproject_toml_path.exists():
+        with open(pyproject_toml_path, 'rb') as f:
+            data = tomllib.load(f)
+            version = data.get('project', {}).get('version')
+
+            print('version', version)
+            if version:
+                return version
+    return '0.0.0'
