82644af81ff2d86522f8c407a4f85de4a81e978f
CodarZ <1476712061@qq.com>
2025-12-10 15:46:08 +0800

    ✨ feat(utils): 新增 datetime 转换工具

---
+++ backend/utils/timezone.py
@@ -0,0 +1,40 @@
+import zoneinfo
+
+from datetime import datetime
+
+from backend.core.config import settings
+
+
+class TimeZone:
+    """时区工具类，根据配置项做统一转换/格式化。"""
+
+    def __init__(self) -> None:
+        self.config_tz = zoneinfo.ZoneInfo(settings.DATETIME_TIMEZONE)
+
+    def now(self) -> datetime:
+        return datetime.now(tz=self.config_tz)
+
+    def format_datetime(self, dt: datetime) -> datetime:
+        """将 datetime 对象转换为当前时区 datetime 对象"""
+        return dt.astimezone(self.config_tz)
+
+    def format_str(self, dt_str: str, fmt: str = settings.DATETIME_FORMAT) -> datetime:
+        """将字符串转为 datetime 对象"""
+        dt = datetime.strptime(dt_str, fmt)
+        return self.to_timezone(dt)
+
+    def to_timezone(self, dt: datetime | int | float) -> datetime:
+        """转换为当前时区 datetime 对象"""
+        if isinstance(dt, (int, float)):
+            return datetime.fromtimestamp(dt, tz=self.config_tz)
+        if dt.tzinfo is None:
+            return dt.replace(tzinfo=self.config_tz)
+        return dt.astimezone(self.config_tz)
+
+    @staticmethod
+    def to_str(dt: datetime, fmt: str = settings.DATETIME_FORMAT) -> str:
+        """将 datetime 对象转换为指定格式字符串"""
+        return dt.strftime(fmt)
+
+
+timezone = TimeZone()
