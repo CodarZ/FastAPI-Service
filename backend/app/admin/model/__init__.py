edd93f16fdd782ae0b4c0746489f292072843688
CodarZ <1476712061@qq.com>
2025-12-18 17:18:46 +0800

    ✨ feat(app/admin): 新增 SysLoginLog 登录日志表

--- backend/app/admin/model/__init__.py
+++ backend/app/admin/model/__init__.py
@@ -1,4 +1,5 @@
 from backend.app.admin.model.dept import SysDept
+from backend.app.admin.model.login_log import SysLoginLog
 from backend.app.admin.model.m2m import sys_role_menu, sys_user_role
 from backend.app.admin.model.menu import SysMenu
 from backend.app.admin.model.operation_log import SysOperationLog
