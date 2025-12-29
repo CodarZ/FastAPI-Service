ddda3c471184569b141bcec9321c916fda5f1050
CodarZ <1476712061@qq.com>
2025-12-29 10:58:27 +0800

    🐛 fix(app/admin): 修复导出角色部门关联表

--- backend/app/admin/model/__init__.py
+++ backend/app/admin/model/__init__.py
@@ -1,4 +1,4 @@
-from backend.app.admin.model.m2m import sys_role_menu, sys_user_role
+from backend.app.admin.model.m2m import sys_role_dept, sys_role_menu, sys_user_role
 from backend.app.admin.model.sys_dept import SysDept
 from backend.app.admin.model.sys_login_log import SysLoginLog
 from backend.app.admin.model.sys_menu import SysMenu
