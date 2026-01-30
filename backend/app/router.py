de8e691e994e88317477613a459512638d50252e
CodarZ <1476712061@qq.com>
2026-01-30 20:02:11 +0800

    🐛 fix(app/admin/monitor): 修复健康检查路由未注册

--- backend/app/router.py
+++ backend/app/router.py
@@ -3,11 +3,13 @@ from backend.common.security.rbac import RBACRouter
 from backend.app.admin.api.auth import router as auth_router
 from backend.app.admin.api.sys import router as sys_router
 from backend.app.admin.api.log import router as log_router
+from backend.app.admin.api.monitor import router as monitor_router
 from backend.common.security.jwt import DependsJWTAuth
 
 
 router = RBACRouter()
 
 router.include_router(auth_router)
+router.include_router(monitor_router)
 router.include_router(sys_router, dependencies=[DependsJWTAuth])
 router.include_router(log_router, dependencies=[DependsJWTAuth])
