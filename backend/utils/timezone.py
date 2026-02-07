e9dbf212be71427a255d2ed6a9efcc92c76f2d75
CodarZ <1476712061@qq.com>
2026-02-07 09:14:15 +0800

    📝 docs(utils): 更新注释和示例

--- backend/utils/timezone.py
+++ backend/utils/timezone.py
@@ -1,37 +1,53 @@
 import zoneinfo
 
-from datetime import datetime
+from datetime import datetime, timezone as dt_timezone
 
 from backend.core.config import settings
 
 
 class TimeZone:
-    def to_utc(self, dt):
-        """将 datetime 对象转换为 UTC 时区 datetime 对象"""
-        from datetime import timezone as dt_timezone
-
-        aware_dt = self.to_timezone(dt)
-        return aware_dt.astimezone(dt_timezone.utc)
-
-    """时区工具类，根据配置项做统一转换/格式化。"""
-
     def __init__(self) -> None:
         self.config_tz = zoneinfo.ZoneInfo(settings.DATETIME_TIMEZONE)
 
     def now(self) -> datetime:
+        """获取项目配置时区 datetime 对象"""
         return datetime.now(tz=self.config_tz)
 
+    def to_utc(self, dt) -> datetime:
+        """将 datetime 对象转换为 UTC 时区 datetime 对象
+
+        Example:
+            输入: 项目配置时区 datetime 对象
+            输出: UTC datetime 对象
+        """
+
+        aware_dt = self.to_timezone(dt)
+        return aware_dt.astimezone(dt_timezone.utc)
+
     def format_datetime(self, dt: datetime) -> datetime:
-        """将 datetime 对象转换为当前时区 datetime 对象"""
+        """将 datetime 对象转换为项目配置时区 datetime 对象
+
+        Example:
+            输入: UTC datetime 对象
+        """
         return dt.astimezone(self.config_tz)
 
     def format_str(self, dt_str: str, fmt: str = settings.DATETIME_FORMAT) -> datetime:
-        """将字符串转为 datetime 对象"""
+        """将字符串转为项目配置时区 datetime 对象
+
+        Example:
+            输入: dt_str='2024-01-01 12:30:45', fmt='%Y-%m-%d %H:%M:%S'
+        """
         dt = datetime.strptime(dt_str, fmt)
         return self.to_timezone(dt)
 
     def to_timezone(self, dt: datetime | int | float) -> datetime:
-        """转换为当前时区 datetime 对象"""
+        """转换为项目配置时区 datetime 对象
+
+        Example:
+            输入: 1704096000  # Unix 时间戳
+            输入: datetime(2024, 1, 1, 12, 0, 0)  # 无时区信息
+        """
         if isinstance(dt, (int, float)):
             return datetime.fromtimestamp(dt, tz=self.config_tz)
         if dt.tzinfo is None:
@@ -40,7 +56,12 @@ class TimeZone:
 
     @staticmethod
     def to_str(dt: datetime, fmt: str = settings.DATETIME_FORMAT) -> str:
-        """将 datetime 对象转换为指定格式字符串"""
+        """将 datetime 对象转换为指定格式字符串
+
+        Example:
+            输入: datetime 对象
+            输出: '2024-01-01 12:30:45'
+        """
         return dt.strftime(fmt)
 
 
