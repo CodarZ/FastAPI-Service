2cafcbc7b13822cfdc61a6ca7908f47111c971ec
CodarZ <1476712061@qq.com>
2026-04-08 17:41:16 +0800

    feat(utils): 新增路由名称唯一性检查和简化操作 ID 功能

--- backend/utils/__init__.py
+++ backend/utils/__init__.py
@@ -1,3 +1,4 @@
 from . import regex
+from .route import ensure_unique_route_name, simplify_operation_id
 
-__all__ = ['regex']
+__all__ = ['ensure_unique_route_name', 'regex', 'simplify_operation_id']
